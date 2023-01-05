767186f4faece294d1a1708de9c6e061ba2c5c91
firephil <philip@firesoft.gr>
2023-01-05 07:07:03 +0200

    2nd problem

---
+++ 2/addtwo.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        pass
+
+
+
+if __name__ == "__main__":
+    l1 = ListNode(2, ListNode(4, ListNode(3)))
+    l2 = ListNode(5, ListNode(6, ListNode(4)))
+    
+    print(Solution().addTwoNumbers(l1, l2))
