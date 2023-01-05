23944efde676d3e2a271138f77a7f54e866d6f84
firephil <philip@firesoft.gr>
2023-01-05 09:42:06 +0200

    scala solution

---
+++ 2/addtwo.scala
@@ -0,0 +1,75 @@
+
+case class ListNode(_x: Int = 0, _next: ListNode = null) {
+   var next: ListNode = _next
+   var x: Int = _x
+   def foreach(f: Int => Unit): Unit = {
+      f(x)
+      if (next != null) next.foreach(f)
+   }
+}
+
+def addTwoNumbers(l1: ListNode, l2: ListNode): ListNode = {
+   var carry = 0
+   var head = new ListNode()
+   var tail = head
+   var p = l1
+   var q = l2
+   while (p != null || q != null) {
+      val x = if (p != null) p.x else 0
+      val y = if (q != null) q.x else 0
+      val sum = carry + x + y
+      carry = sum / 10
+      tail.next = new ListNode(sum % 10)
+      tail = tail.next
+      if (p != null) p = p.next
+      if (q != null) q = q.next
+
+        if (carry > 0){
+            tail.next = new ListNode(carry)
+        }
+   }
+   head.next
+}
+
+
+def add2Numbers(l1: ListNode, l2: ListNode): ListNode = {
+   var carry = 0
+   var head = new ListNode()
+   var tail = head
+   var p = l1
+   var q = l2
+   
+   while (p != null || q != null) {
+      val x = if (p != null) p.x else 0
+      val y = if (q != null) q.x else 0
+      var sum = carry + x + y
+      
+      if (sum > 9) {
+        carry = 1
+        sum = sum - 10
+      }
+      else 
+        carry = 0
+
+      tail.next = new ListNode(sum)
+      tail = tail.next
+      if (p != null) p = p.next
+      if (q != null) q = q.next
+
+        if (carry > 0){
+            tail.next = new ListNode(carry)
+        }
+   }
+   head.next
+}
+
+
+
+
+
+@main 
+def hello() = 
+    val l1 = ListNode(2, ListNode(4, ListNode(3)))
+    val l2 = ListNode(5, ListNode(6, ListNode(4)))
+    val res = add2Numbers (l1,l2)
+    res.foreach(print)
